d8d17f1e5c23f282d13876154d281554a96bc878
Blankeos <carloantonioct@gmail.com>
2024-05-06 23:20:03 +0800

    fix: Fixed configs.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "drizzle-kit";
 import "dotenv/config";
 
 export default {
-  schema: "./src/server/schema/auth.ts",
+  schema: "./src/server/schema",
   out: "./migrations",
   driver: "libsql", // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
   dbCredentials: {
--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,11 @@
 export const config = {
-  port: (Bun.env.PORT || 3000) as number,
-  node_env: Bun.env.NODE_ENV ?? ("development" as "development" | "production"),
+  port: (process.env.PORT || 3000) as number,
+  node_env:
+    process.env.NODE_ENV ?? ("development" as "development" | "production"),
   database: {
     /** The url of the database. */
-    url: Bun.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
     /** Not needed in development. https://docs.turso.tech/local-development#sqlite */
-    authToken: Bun.env.DATABASE_AUTH_TOKEN!,
+    authToken: process.env.DATABASE_AUTH_TOKEN!,
   },
 };
