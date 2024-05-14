982748dc83fac546c1992e770d401ce5b02cdd41
Blankeos <carloantonioct@gmail.com>
2024-05-15 00:29:17 +0800

    chore: Way better config defaults.

--- src/config.private.ts
+++ src/config.private.ts
@@ -1,9 +1,7 @@
-export const config = {
+/** Only place private configurations here. */
+export const privateConfig = {
   /** Port of the app (in dev). */
   PORT: (import.meta.env.PORT || 3000) as number,
-  /** The base origin of the app. */
-  PUBLIC_ENV__BASE_ORIGIN:
-    import.meta.env.PUBLIC_ENV__BASE_ORIGIN || "http://localhost:3000",
   /** Development or Production. */
   NODE_ENV: (import.meta.env.NODE_ENV ?? "development") as
     | "development"
@@ -15,11 +13,17 @@ export const config = {
     /** Not needed in development. https://docs.turso.tech/local-development#sqlite */
     AUTH_TOKEN: import.meta.env.DATABASE_AUTH_TOKEN!,
   },
+  /** S3Bucket-specific settings. */
   s3: {
-    APPLICATION_KEY: import.meta.env.S3_APPLICATION_KEY!,
-    APPLICATION_KEY_ID: import.meta.env.S3_APPLICATION_KEY_ID!,
-    BUCKET_NAME: import.meta.env.S3_BUCKET_NAME!,
-    REGION: import.meta.env.S3_REGION!,
-    ENDPOINT: import.meta.env.S3_ENDPOINT!,
+    /** Application Key ID in B2 (accessKeyId in S3) */
+    ACCESS_KEY_ID: import.meta.env.S3_ACCESS_KEY_ID!,
+    /** Application Key in B2 (secretAccessKey in S3) */
+    SECRET_ACCESS_KEY: import.meta.env.S3_SECRET_ACCESS_KEY!,
+    /** Name of the bucket. */
+    BUCKET_NAME: import.meta.env.S3_BUCKET_NAME! ?? "spend-snap",
+    /** Region of the bucket. */
+    REGION: import.meta.env.S3_REGION! ?? "us-east-1",
+    /** URL of the bucket. */
+    ENDPOINT: import.meta.env.S3_ENDPOINT! ?? "http://localhost:4566",
   },
 };
