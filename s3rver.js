73038cfaf3e8a917332ea0c15bb4e99a9589cfa7
Blankeos <carloantonioct@gmail.com>
2024-05-17 03:22:07 +0800

    chore: Added s3 mocking.

---
+++ s3rver.js
@@ -0,0 +1,18 @@
+// https://www.youtube.com/watch?v=T7iNytkGIiQ
+const fs = require("fs");
+const corsConfig = require.resolve("s3rver/example/cors.xml");
+const S3rver = require("s3rver");
+let instance;
+
+new S3rver({
+  port: 9000,
+  address: "127.0.0.1",
+  silent: false,
+  directory: "./data/s3",
+  configureBuckets: [
+    {
+      name: "spend-snap",
+      configs: [fs.readFileSync(corsConfig)],
+    },
+  ],
+}).run();
