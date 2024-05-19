81f6e65fc6c988b0f7ac2823b61dc8b33d3a98d0
Blankeos <carloantonioct@gmail.com>
2024-05-19 21:54:39 +0800

    fix: Fixed errors for new vike.

--- src/pages/_error/+Page.tsx
+++ src/pages/_error/+Page.tsx
@@ -1,14 +1,33 @@
+import Button from "@/components/Button";
 import { Match, Show, Switch } from "solid-js";
+import { usePageContext } from "vike-solid/usePageContext";
+import { navigate } from "vike/client/router";
 
-export default function Page(props: { is404: boolean; errorInfo?: string }) {
+export default function Page() {
+  const pageContext = usePageContext();
+
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        <Switch fallback={"500 Internal Server Error"}>
-          <Match when={props.is404}>404 Page Not Found</Match>
-        </Switch>
-      </h1>
-      <p class="mt-8">
+    <main class="text-center mx-auto text-gray-700 p-4 h-[90vh] flex flex-col items-center justify-center">
+      <Switch fallback={"500 Internal Server Error"}>
+        <Match when={pageContext.is404}>
+          <div class="flex flex-col gap-y-6">
+            <h1 class="max-6-xs text-4xl text-gray-800 font-bold">
+              Page not found
+            </h1>
+            <p>Sorry, we couldn't find the page you're looking for.</p>
+            <div class="flex gap-y-2 justify-center items-center">
+              <Button
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                Go back
+              </Button>
+            </div>
+          </div>
+        </Match>
+      </Switch>
+      {/* <p class="mt-8">
         Visit{" "}
         <a
           href="https://solidjs.com"
@@ -27,7 +46,7 @@ export default function Page(props: { is404: boolean; errorInfo?: string }) {
         <a href="/about" class="text-sky-600 hover:underline">
           About Page
         </a>
-      </p>
+      </p> */}
     </main>
   );
 }
