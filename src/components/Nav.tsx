0fec679595fd1ef8e8011cafe6574e4eb7dd7e20
Blankeos <carloantonioct@gmail.com>
2024-05-19 22:29:51 +0800

    feat: Better nav colors.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -29,20 +29,28 @@ export default function Nav() {
 
   const active = (path: string) =>
     path == pageContext.urlPathname
-      ? "border-sky-600"
-      : "border-transparent hover:border-sky-600";
+      ? "border-purple-400"
+      : "border-transparent hover:border-purple-400";
 
   return (
-    <nav class="bg-primary">
-      <ul class="container flex items-center p-3 text-gray-200">
+    <nav class="bg-primary flex flex-col gap-y-2 items-center p-3">
+      <div class="relative text-white font-bold items-start">
+        <span>Spend Snap</span>
+        <span class="absolute -top-1 left-[102%] bg-purple-500 text-white rounded-sm text-xs py-[1px] px-1 border-purple-200 border">
+          Beta
+        </span>
+      </div>
+      <ul class="container flex items-center text-gray-200 justify-center">
         <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
           <a href="/">Home {authStore().loading}</a>
         </li>
-        <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
+        {/* <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
           <a href="/about">About</a>
-        </li>
+        </li> */}
         <Show when={authStore().loading}>
-          <IconLoading />
+          <li class="px-2">
+            <IconLoading />
+          </li>
         </Show>
         <Show when={!authStore().user && !authStore().loading}>
           <li class={`border-b-2 ${active("/login")} mx-1.5 sm:mx-6`}>
