54f975801bf1c1b1eaad3156a69be1a488948c37
Blankeos <carloantonioct@gmail.com>
2024-05-19 22:39:58 +0800

    feat: Better view image modal.

--- src/components/modals/ViewImageModal.tsx
+++ src/components/modals/ViewImageModal.tsx
@@ -1,4 +1,4 @@
-import { Show, VoidProps } from "solid-js";
+import { createEffect, createSignal, Show, VoidProps } from "solid-js";
 import Modal, { createModalOpeners } from "./Modal";
 import { createQuery } from "@tanstack/solid-query";
 import { hc } from "@/lib/honoClient";
@@ -9,6 +9,8 @@ type ViewImageModalProps = {
 };
 
 export default function ViewImageModal(props: VoidProps<ViewImageModalProps>) {
+  const [imageHasLoaded, setImageHasLoaded] = createSignal(false);
+
   const receiptImageUrlQuery = createQuery(() => ({
     queryKey: ["view-image-modal", props.imgObjectKey],
     queryFn: async () => {
@@ -23,13 +25,34 @@ export default function ViewImageModal(props: VoidProps<ViewImageModalProps>) {
         return null;
       }
     },
+    staleTime: Infinity, // Always cache the result of the query
     enabled: !!props.imgObjectKey,
   }));
 
+  createEffect(() => {
+    // Whenever imgObjectKey changes, setImageHasLoaded to false.
+    if (
+      props.imgObjectKey &&
+      !receiptImageUrlQuery.data &&
+      receiptImageUrlQuery.isFetching
+    ) {
+      setImageHasLoaded(false);
+    }
+  });
+
   return (
     <Modal id="view-image-modal">
-      <Show when={receiptImageUrlQuery.isFetching}>
-        <IconLoading />
+      <Show
+        when={
+          receiptImageUrlQuery.isLoading ||
+          receiptImageUrlQuery.isFetching ||
+          !imageHasLoaded()
+        }
+      >
+        <div class="flex gap-x-2 items-center">
+          <IconLoading />
+          <span>Loading Image...</span>
+        </div>
       </Show>
       <Show
         when={receiptImageUrlQuery?.data && !receiptImageUrlQuery.isFetching}
@@ -38,6 +61,7 @@ export default function ViewImageModal(props: VoidProps<ViewImageModalProps>) {
           src={receiptImageUrlQuery?.data!}
           alt="receipt preview"
           class="h-full w-full"
+          onload={() => setImageHasLoaded(true)}
         />
       </Show>
     </Modal>
