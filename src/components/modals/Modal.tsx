51607a9aa4a2f9cc65dc718fb9aaf4e40292cc68
Blankeos <carloantonioct@gmail.com>
2024-05-19 22:38:33 +0800

    feat: Complex Modal usecase.

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -1,5 +1,7 @@
 import { cn } from "@/lib/cn";
-import { FlowProps, JSXElement, Show } from "solid-js";
+import { createSignal, FlowProps, JSXElement, Show } from "solid-js";
+import { T } from "../../../dist/server/chunks/chunk-CJJHhQrS";
+import { createStore } from "solid-js/store";
 
 type ModalProps = {
   id: string;
@@ -19,8 +21,16 @@ type ModalProps = {
   /**
    * Apart from the document.getElementById().showModal and .close(),
    * this directly attaches the class that makes this open.
+   *
+   * This is recommend especially when rendering toasts in dialog. Because
+   * dialog has a bug with Z-Index: github.com/saadeghi/daisyui/issues/2858
    */
-  forceOpen?: boolean;
+  isOpen?: boolean;
+
+  /**
+   * Callback when the close or outside (modal-backdrop) is clicked.
+   */
+  onCloseClick?: () => void;
 };
 
 export default function Modal(props: FlowProps<ModalProps>) {
@@ -30,7 +40,7 @@ export default function Modal(props: FlowProps<ModalProps>) {
       class={cn(
         "modal modal-bottom sm:modal-middle",
         props.class,
-        props.forceOpen && "modal-open"
+        props.isOpen && "modal-open"
       )}
     >
       <div class="modal-box">
@@ -39,13 +49,33 @@ export default function Modal(props: FlowProps<ModalProps>) {
           <div class="modal-action">{props.modalActions}</div>
         </Show>
       </div>
-      <form method="dialog" class="modal-backdrop">
+      <form method="dialog" class="modal-backdrop" onClick={props.onCloseClick}>
         <button>close</button>
       </form>
     </dialog>
   );
 }
 
+/** Use for complex cases, like an edit form modal with default values. */
+export function createModalOpener<T>(modalData: T) {
+  const [_modalData, _setModalData] = createSignal<T | null>(modalData);
+
+  function openModal(modalData: T) {
+    _setModalData((_) => modalData);
+  }
+
+  function closeModal() {
+    _setModalData(null);
+  }
+
+  return {
+    modalData: _modalData,
+    openModal,
+    closeModal,
+  };
+}
+
+/** Use for simple cases. */
 export function createModalOpeners(id: string) {
   return {
     // @ts-ignore
